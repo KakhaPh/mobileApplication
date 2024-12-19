39c841982c49fdef9b8111640236b4dacf13bcdf
unknown <kakha.putkaradze@eu.edu.ge>
2024-12-19 11:54:45 +0400

    homework_4 initial commit

---
+++ homework_4/app/src/main/java/com/project/homework_4/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.project.homework_4
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.project.homework_4.ui.theme.Homework_4Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Homework_4Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Kakha",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    Homework_4Theme {
+//        Greeting("Android")
+//    }
+//}
---
+++ homework_4/app/src/main/java/com/project/homework_4/presentation/screens/splashScreen.kt
@@ -0,0 +1,2 @@
+package com.project.homework_4.presentation.screens
+
