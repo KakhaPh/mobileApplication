56e55295bb154f49a4305c6bf4073fc6a76241a1
KakhaPh <kakha.putkaradze@eu.edu.ge>
2025-01-01 22:41:02 +0400

    Add screens - SplashScreen, FirstScreen | viewmodel Folder

---
+++ cryptocurrencyapp/app/src/main/java/com/project/cryptocurrencyapp/presentation/screen/SplashScreen.kt
@@ -0,0 +1,34 @@
+package com.project.cryptocurrencyapp.presentation.screen
+
+import android.window.SplashScreen
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.project.cryptocurrencyapp.presentation.navigation.Routes
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(navController: NavController) {
+    LaunchedEffect(key1 = true) {
+        delay(2000L)
+        navController.navigate(Routes.FirstScreen.routes) {
+            popUpTo(Routes.SplashScreen.routes) { inclusive = true}
+        }
+    }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Crypto Currency App",
+            style = MaterialTheme.typography.headlineMedium
+        )
+    }
+}
